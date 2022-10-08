33b2a3ec131deb804f45144bf1bb29ef86df2a99
VictorSanchezIT <115325463+VictorSanchezIT@users.noreply.github.com>
2022-10-07 22:49:29 -0500

    Agrego comentario

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
     printf("Hola mundo/n");
-    printf("/nLinea editada desde GitHub");
+    printf("/nLinea editada desde GitHub"); //Orale va, otra linea editada desde GitHub
     return 0;
 }
